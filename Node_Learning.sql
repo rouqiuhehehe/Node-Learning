b9562de3516fc4954743392ee76016873adf7ad4
dongsichong <dongsichong@mucang.cn>
2021-07-15 19:08:07 +0800

    feat: sql and webpack

---
+++ Node_Learning.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: node_learned
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `articles`
+--
+
+DROP TABLE IF EXISTS `articles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `articles` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) NOT NULL DEFAULT '',
+  `content` varchar(200) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `articles`
+--
+
+LOCK TABLES `articles` WRITE;
+/*!40000 ALTER TABLE `articles` DISABLE KEYS */;
+INSERT INTO `articles` VALUES (3,'news','通过 mysql> 命令窗口可以很简单的创建MySQL数据表。你可以使用 SQL 语句 CREATE TABLE 来创建数据表。'),(4,'news','通过 mysql> 命令窗口可以很简单的创建MySQL数据表。你可以使用 SQL 语句 CREATE TABLE 来创建数据表。'),(5,'news','通过 mysql> 命令窗口可以很简单的创建MySQL数据表。你可以使用 SQL 语句 CREATE TABLE 来创建数据表。'),(6,'mysql学习','如果你不想字段为 NULL 可以设置字段的属性为 NOT NULL， 在操作数据库时如果输入该字段的数据为NULL ，就会报错。');
+/*!40000 ALTER TABLE `articles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-15 19:07:21
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,3 +1,4 @@
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import * as path from 'path';
 import * as webpack from 'webpack';
 // in case you run into any typescript error when configuring `devServer`
@@ -11,43 +12,16 @@ const config: webpack.Configuration = {
         filename: 'app.bundle.js'
     },
     module: {
-        rules: [
-            { test: /\.tsx?$/, use: 'ts-loader' },
-            {
-                test: /\.png/,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.html/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'static/[hash][ext][query]' // 单独指定 名字
-                }
-            },
-            {
-                test: /\.svg/,
-                type: 'asset/inline' // inline 的时候不需要指定文件名
-            },
-            {
-                test: /\.txt/,
-                type: 'asset',
-                parser: {
-                    dataUrlCondition: {
-                        // tslint:disable-next-line:no-magic-numbers
-                        maxSize: 4 * 1024 // 4kb  指定大小
-                    }
-                }
-            }
-        ]
+        rules: [{ test: /\.tsx?$/, use: 'ts-loader' }]
     },
     resolve: {
         extensions: ['.ts', '.js', '.json'],
         fallback: {
             path: require.resolve('path-browserify'),
             url: require.resolve('url'),
-            buffer: require.resolve('buffer/'),
-            util: require.resolve('util/'),
-            stream: require.resolve('stream-browserify/')
+            buffer: require.resolve('buffer'),
+            util: require.resolve('util')
+            // stream: require.resolve('stream-browserify')
         },
         alias: {
             '@types': path.resolve(__dirname, './src/types/'),
@@ -62,6 +36,14 @@ const config: webpack.Configuration = {
         }),
         new webpack.ProvidePlugin({
             Buffer: ['buffer', 'Buffer']
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, './src/assets'),
+                    to: path.join(__dirname, './build/assets')
+                }
+            ]
         })
     ]
 };
